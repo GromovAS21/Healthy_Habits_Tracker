f538a3f0c1f8e73e2b67fee83de252ee84ea8216
Anton Gromov <seed14112@yandex.ru>
2024-09-29 14:23:17 +0300

    Добавление команды для создания тестовых пользователей

---
+++ users/management/commands/cu.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """
+    Команда для создания пользователей
+    """
+    def handle(self, *args, **options):
+        users_list = [
+            {"email": "test@test.ru"},
+            {"email": "test1@test.ru"}
+        ]
+
+        users_for_create = []
+
+        for user in users_list:
+            users_for_create.append(User(**user))
+            users_for_create[-1].set_password("Qwerty")
+
+        User.objects.bulk_create(users_for_create)
